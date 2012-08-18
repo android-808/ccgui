a5785687c1339c5064453fbd2e944b37714c0366
Robert Irvin <robert@localhost.localdomain>
2012-08-18 17:35:10 +0100

    Add support for Ring mode options
    Adds support for all options except temperature unit. Like Cairo, media player support may need validation.

--- ccgui/ring.py
+++ ccgui/ring.py
@@ -14,8 +14,15 @@ class RingPage(OptionsPage):
         l = ccgui.ui.add_label(self, "Descriptive text")
         ccgui.ui.add_wide_control(self,l)
         
-        l = ccgui.ui.add_label(self, "CPU core selector")
-        ccgui.ui.add_wide_control(self,l)
+        l = ccgui.ui.add_label(self, "Number of CPU cores:")
+        #l.set_margin_left(18)
+        adjustment = Gtk.Adjustment(1, 1, 16, 1, 4, 0)
+        self.cpucount = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
+        self.cpucount.set_adjustment(adjustment)
+        ccgui.ui.add_row(self, l, self.cpucount)
+
+        self.network = ccgui.ui.add_check_button(self, "Enable network monitor.")
+        ccgui.ui.add_wide_control(self,self.network)
         
         ccgui.ui.add_divider(self)        
         ccgui.ui.add_section_title(self, "Media Player Support")
@@ -23,31 +30,31 @@ class RingPage(OptionsPage):
         ccgui.ui.add_wide_control(self,l)
         
         #Reuse options for each player.
-        playermodes = Gtk.ListStore(str)
-        playermodes.append(["Disabled"])
-        playermodes.append(["Ring"])
-        playermodes.append(["Ring-Case"])
-        playermodes.append(["Ring-CD"])
-        playermodes.append(["Ring-Glassy"])
+        self.playerstyles = Gtk.ListStore(str, str)
+        self.playerstyles.append(["Disabled", ""])
+        self.playerstyles.append(["Ring", "ring"])
+        self.playerstyles.append(["Ring-Case", "ring-case"])
+        self.playerstyles.append(["Ring-CD", "ring-cd"])
+        self.playerstyles.append(["Ring-Glassy", "ring-glassy"])
         
         l = ccgui.ui.add_label(self, "Banshee:")
         self.banshee = Gtk.ComboBoxText(halign = Gtk.Align.START,
                                         hexpand=True)
-        self.banshee.set_model(playermodes)
+        self.banshee.set_model(self.playerstyles)
         self.banshee.set_active(0)
         ccgui.ui.add_row(self, l, self.banshee)
         
         l = ccgui.ui.add_label(self, "Clementine:")
         self.clementine = Gtk.ComboBoxText(halign = Gtk.Align.START,
                                            hexpand=True)
-        self.clementine.set_model(playermodes)
+        self.clementine.set_model(self.playerstyles)
         self.clementine.set_active(0)
         ccgui.ui.add_row(self, l, self.clementine)
         
         l = ccgui.ui.add_label(self, "Rhythmbox:")
         self.rhythmbox = Gtk.ComboBoxText(halign = Gtk.Align.START,
                                          hexpand=True)
-        self.rhythmbox.set_model(playermodes)
+        self.rhythmbox.set_model(self.playerstyles)
         self.rhythmbox.set_active(0)
         ccgui.ui.add_row(self, l, self.rhythmbox)
  
@@ -63,8 +70,24 @@ class Ring():
         return self.ring.page_number
         
     def GetOptions(self):
-        ret = str()
+        ret = str(" --ring")
+        ret += " --cpu=" + str(self.ring.cpucount.get_value_as_int())
+        
+        if self.ring.network.get_active() == True:
+            ret += " --network"
+            
+        if self.ring.banshee.get_active() != 0:
+            tree_iter = self.ring.banshee.get_active_iter()
+            ret += " --banshee=" + self.ring.playerstyles[tree_iter][1]
+        
+        if self.ring.clementine.get_active() != 0:
+            tree_iter = self.ring.clementine.get_active_iter()
+            ret += " --clementine=" + self.ring.playerstyles[tree_iter][1]
 
+        if self.ring.rhythmbox.get_active() != 0:
+            tree_iter = self.ring.rhythmbox.get_active_iter()
+            ret += " --rhythmbox=" + self.ring.playerstyles[tree_iter][1]
+        
         return ret
         
         
