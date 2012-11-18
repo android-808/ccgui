43bb569f4f93e323bc06fd1dcaa79885c5cc1333
Robert Irvin <android_808@hotmail.com>
2012-11-18 19:24:01 +0000

    Add support for most features available in conky-colors default mode.
    This patch set adds support for all features in default mode except hddtemp and Debian/Ubuntu update support. Adds some checks so that options are disabled if they rely on another feature that is currently disabled.  Replaces some place holder widgets so that they can be accessed via the parent class.

--- ccgui/default.py
+++ ccgui/default.py
@@ -1,5 +1,3 @@
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
 from gi.repository import Gtk
 import ccgui.common
 import ccgui.ui
@@ -11,7 +9,7 @@ class SystemPage(OptionsPage):
         
         ccgui.ui.add_section_title(self, "System Information")
         
-        l = ccgui.ui.add_label(self, "Descriptive text")
+        l = ccgui.ui.add_label(self, "Configures system usage reporting.")
         ccgui.ui.add_wide_control(self,l)
         
         self.cpu = ccgui.ui.add_check_button(self, "Enable CPU usage monitor.")
@@ -84,8 +82,20 @@ class DesktopPage(OptionsPage):
         
         l = ccgui.ui.add_label(self, "This allows you to specify desktop specific options.")
         ccgui.ui.add_wide_control(self,l)
+
+        l = ccgui.ui.add_label(self, "Side of the screen to display Conky:")
+        self.sides = Gtk.ListStore(str, str)
+
+        self.sides.append(["Left", "left"])
+        self.sides.append(["Right", "right"])
+        self.side = Gtk.ComboBoxText(halign = Gtk.Align.START,
+                                                   hexpand=True)
+        self.side.set_model(self.sides)
+        self.side.set_active(1)
+        ccgui.ui.add_row(self, l, self.side)
         
         self.replacelogo = ccgui.ui.add_check_button(self, "Replace monitor icon with distribution/desktop logo.")
+        self.replacelogo.connect("toggled", self.on_logo_toggled) 
         ccgui.ui.add_wide_control(self,self.replacelogo)
         
         l = ccgui.ui.add_label(self, "Distribution/Desktop:")
@@ -107,7 +117,11 @@ class DesktopPage(OptionsPage):
                                                    hexpand=True)
         self.icon.set_model(self.icons)
         self.icon.set_active(0)
+        self.icon.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.icon)
+
+    def on_logo_toggled(self, button):
+        self.icon.set_sensitive(button.get_active())
 
 class DateTimePage(OptionsPage):
     def __init__(self, assistant):
@@ -268,22 +282,37 @@ class HDDPage(OptionsPage):
 
         ccgui.ui.add_divider(self)
         ccgui.ui.add_section_title(self, "HDD Temperature Monitoring")
-        l = ccgui.ui.add_label(self, "Feature explanation")
+        l = ccgui.ui.add_label(self, "Feature explanation (NOT IMPLEMENTED)")
         ccgui.ui.add_wide_control(self,l)
+
+        self.hddtemp = ccgui.ui.add_check_button(self, "Enable HDD temperature monitoring.")
+        self.hddtemp.connect("toggled", self.on_hddtemp_toggled)
+        ccgui.ui.add_wide_control(self,self.hddtemp)
 
         l = ccgui.ui.add_label(self, "HDD Temperature 1:")
         self.hdtemp1 = ccgui.ui.add_entry(self)
+        self.hdtemp1.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.hdtemp1)
         l = ccgui.ui.add_label(self, "HDD Temperature 2:")
         self.hdtemp2 = ccgui.ui.add_entry(self)
+        self.hdtemp2.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.hdtemp2)
         l = ccgui.ui.add_label(self, "HDD Temperature 3:")
         self.hdtemp3 = ccgui.ui.add_entry(self)
+        self.hdtemp3.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.hdtemp3)
         l = ccgui.ui.add_label(self, "HDD Temperature 4:")
         self.hdtemp4 = ccgui.ui.add_entry(self)
+        self.hdtemp2.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.hdtemp4)
-     
+
+    def on_hddtemp_toggled(self, button):
+        state = button.get_active()
+        self.hdtemp1.set_sensitive(state)
+        self.hdtemp2.set_sensitive(state)
+        self.hdtemp3.set_sensitive(state)
+        self.hdtemp4.set_sensitive(state)
+         
 class NetworkPage(OptionsPage):
     def __init__(self, assistant):
         OptionsPage.__init__(self, assistant)
@@ -293,6 +322,7 @@ class NetworkPage(OptionsPage):
         ccgui.ui.add_wide_control(self,l)
 
         self.network = ccgui.ui.add_check_button(self, "Enable network monitoring.") 
+        self.network.connect("toggled", self.on_network_toggled)
         ccgui.ui.add_wide_control(self, self.network)
 
         l = ccgui.ui.add_label(self, "Change default device number:")
@@ -301,38 +331,55 @@ class NetworkPage(OptionsPage):
         
         l = ccgui.ui.add_label(self, "Ethernet (/dev/ethX):")
         self.ethx = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
-        ethadjustment = Gtk.Adjustment(1, 1, 10, 1, 4, 0)        
+        ethadjustment = Gtk.Adjustment(0, 0, 10, 1, 4, 0)        
         self.ethx.set_adjustment(ethadjustment)
+        self.ethx.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.ethx)
 
         l = ccgui.ui.add_label(self, "Wireless (/dev/wlanX):")
-        self.ethx = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
-        wlanadjustment = Gtk.Adjustment(1, 1, 10, 1, 4, 0)
-        self.ethx.set_adjustment(wlanadjustment)
-        ccgui.ui.add_row(self, l, self.ethx)
+        self.wlanx = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
+        wlanadjustment = Gtk.Adjustment(0, 0, 10, 1, 4, 0)
+        self.wlanx.set_adjustment(wlanadjustment)
+        self.wlanx.set_sensitive(False)
+        ccgui.ui.add_row(self, l, self.wlanx)
 
         l = ccgui.ui.add_label(self, "PPP/3G Modem (/dev/pppX):")
-        self.ethx = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
-        pppadjustment = Gtk.Adjustment(1, 1, 10, 1, 4, 0)
-        self.ethx.set_adjustment(pppadjustment)
-        ccgui.ui.add_row(self, l, self.ethx)        
+        self.pppx = Gtk.SpinButton(halign = Gtk.Align.START, hexpand = True)
+        pppadjustment = Gtk.Adjustment(0, 0, 10, 1, 4, 0)
+        self.pppx.set_adjustment(pppadjustment)
+        self.pppx.set_sensitive(False)        
+        ccgui.ui.add_row(self, l, self.pppx)        
 
         ccgui.ui.add_divider(self)
         ccgui.ui.add_section_title(self, "GMail")
         
-        l = ccgui.ui.add_label(self, "Descriptive text")
+        l = ccgui.ui.add_label(self, "Reports the number of new messages in the specified account.\n\n<b>Warning:</b> Details are stored in plain text!")
         ccgui.ui.add_wide_control(self,l)
 
-        self.gmail = ccgui.ui.add_check_button(self, "Enable GMail support.") 
+        self.gmail = ccgui.ui.add_check_button(self, "Enable GMail support.")
+        self.gmail.connect("toggled", self.on_gmail_toggled)
         ccgui.ui.add_wide_control(self, self.gmail)
         
         l = ccgui.ui.add_label(self, "Username:")
         self.username = ccgui.ui.add_entry(self)
+        self.username.set_sensitive(False)        
         ccgui.ui.add_row(self, l, self.username)
         
         l = ccgui.ui.add_label(self, "Password:")
         self.password = ccgui.ui.add_entry(self)
+        self.password.set_sensitive(False)
         ccgui.ui.add_row(self, l, self.password)
+
+    def on_network_toggled(self, button):
+        state = button.get_active()
+        self.ethx.set_sensitive(state)
+        self.wlanx.set_sensitive(state)
+        self.pppx.set_sensitive(state)
+
+    def on_gmail_toggled(self, button):
+        state = button.get_active()
+        self.username.set_sensitive(state)
+        self.password.set_sensitive(state)
         
 class WeatherPage(OptionsPage):
     def __init__(self, assistant):
@@ -346,8 +393,8 @@ class WeatherPage(OptionsPage):
         l = ccgui.ui.add_label(self, "Weather Mode:")
         self.weatherprovider = Gtk.ListStore(str, str)
         self.weatherprovider.append(["Disabled", ""])
-        self.weatherprovider.append(["BBC Weather", "--bbcweather"])
-        self.weatherprovider.append(["Yahoo Weather", "--weather"])
+        self.weatherprovider.append(["BBC Weather", " --bbcweather"])
+        self.weatherprovider.append(["Yahoo Weather", " --weather"])
         self.weather = Gtk.ComboBoxText(halign = Gtk.Align.START,
                                       hexpand=True)
         self.weather.set_model(self.weatherprovider)
@@ -355,8 +402,8 @@ class WeatherPage(OptionsPage):
         ccgui.ui.add_row(self, l, self.weather)
         
         l = ccgui.ui.add_label(self, "Area code:")
-        w = ccgui.ui.add_entry(self)
-        ccgui.ui.add_row(self, l, w)   
+        self.area = ccgui.ui.add_entry(self)
+        ccgui.ui.add_row(self, l, self.area)   
 
 class Default():
     def __init__(self, assistant):
@@ -376,22 +423,84 @@ class Default():
         
     def GetOptions(self):
         ret = str()
+        # System Information
         if self.system.cpu.get_active() == True:
             ret += " --cpu=" + str(self.system.cpucount.get_value_as_int())
         if self.system.cputemp.get_active() == True:
             ret += " --cputemp"
         if self.system.swap.get_active() == True:
             ret += " --swap"
+        if self.system.processmonitor.get_active() == True:
+            ret += " --proc=" + str(self.system.processcount.get_value_as_int())
+        # Battery Information        
         if self.system.battery.get_active() == True:
             ret += " --battery"
-            
+            ret += " --battery-value=" + str(self.system.batteryx.get_value_as_int())
+        # Nvidia
+        if self.system.nvidia.get_active() == True:
+            ret += " --nvidia"   
+        
         # Desktop
+        tree_iter = self.desktop.side.get_active_iter()
+        ret += " --side=" + self.desktop.sides[tree_iter][1]
+
         if self.desktop.replacelogo.get_active() != 0:
             tree_iter = self.desktop.icon.get_active_iter()
             ret += self.desktop.icons[tree_iter][1]
         
+        # Date/Time
+        if self.datetime.nodata.get_active() == True:
+            ret += " --nodata"
+        else:
+            tree_iter = self.datetime.clock.get_active_iter()
+            ret += " --clock=" + self.datetime.clockstyles[tree_iter][1]        
+            
+            if self.datetime.calendar.get_active() != 0:
+                tree_iter = self.datetime.calendar.get_active_iter()
+                ret += self.datetime.calendarstyles[tree_iter][1]
+            if self.datetime.task.get_active() == True:
+                ret += " --task"
+
+        # Media Support
+        tree_iter = self.media.photo.get_active_iter()
+        ret += self.media.photostyles[tree_iter][1]  
+
+        if self.media.banshee.get_active() != 0:
+            tree_iter = self.media.banshee.get_active_iter()
+            ret += " --banshee=" + self.media.playerstyles[tree_iter][1]
+        if self.media.clementine.get_active() != 0:
+            tree_iter = self.media.clementine.get_active_iter()
+            ret += " --clementine=" + self.media.playerstyles[tree_iter][1]
+        if self.media.rhythmbox.get_active() != 0:
+            tree_iter = self.media.rhythmbox.get_active_iter()
+            ret += " --banshee=" + self.media.playerstyles[tree_iter][1]
+        if self.media.mpd.get_active() == True:
+            ret += " --mpd" 
+        if self.media.covergloobus.get_active() == True:
+            ret += " --covergloobus" 
+
+        # HDD
+        if self.hdd.hdd.get_active() == True:
+            tree_iter = self.hdd.hdd.get_active_iter()
+            ret += " --hdd=" + self.hdd.hddstyles[tree_iter][1]
+
+        # Network
+        if self.network.network.get_active() == True:
+            ret += " --network"
+            ret += " --ethx=" + str(self.network.ethx.get_value_as_int())
+            ret += " --wlanx=" + str(self.network.wlanx.get_value_as_int())
+            ret += " --pppx=" + str(self.network.pppx.get_value_as_int())
+
+        if self.network.gmail.get_active() == True:
+            ret += " --gmail"            
+            ret += " --username=" + self.network.username.get_text()
+            ret += " --password=" + self.network.password.get_text()
+
+        # Finally, Weather
+        if self.weather.weather.get_active() != 0:
+            tree_iter = self.weather.weather.get_active_iter()
+            ret += self.weather.weatherprovider[tree_iter][1] + "=" + self.weather.area.get_text()
+
         return ret
         
-        
-        
-        
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
